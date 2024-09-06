9e4f07f5aefab740ec5ba0f95b531fb81d81cac8
themanprince <princeadigwe29@gmail.com>
2024-09-06 20:13:39 +0000

    okaaay

--- server.py
+++ server.py
@@ -1,15 +1,20 @@
 #smylerCM server
 from flask import Flask, render_template
-
+import uuid
+from StorageHandler import StorageHandler
 
 
 app = Flask(__name__)
+store = StorageHandler()
 
 @app.route("/home", methods=["GET"])
 def home():
     return render_template("index.html")
 
 
-@app.route("/gen-link", methods=["GET"])
-def genLink():
-    pass
+@app.route("/process-order", methods=["GET"])
+def process_order():
+    id = uuid.uuid4()
+    store.storeID(id)
+    link = f"localhost:5000/verify?id={id}"
+    return render_template("barcode.html", link=link)
